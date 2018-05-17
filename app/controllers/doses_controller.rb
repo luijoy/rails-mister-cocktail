341d1934a058d7a0ce3edc781e45a055f7ebaf7c
Lui Kohl <lui.kohl@gmail.com>
2018-05-17 15:50:44 +0200

    mostly done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,40 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    respond_to do |format|
+      if @dose.save
+        format.html { redirect_to cocktail_path(@cocktail), notice: 'Cocktail was successfully created.' }
+      else
+        @dose
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    # redirecting to the index
+    redirect_to dose_path
+  end
+
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses, only: [ :new, :create ]
+  end
+  resources :doses, only: [ :destroy ]
 end
+
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   resources :restaurants do
+#     resources :reviews, only: [ :index, :new, :create ]
+#   end
+# end
