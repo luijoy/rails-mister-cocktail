9b9621244555a72f57a9a13064f1d5fa60412662
Lui Kohl <lui.kohl@gmail.com>
2018-05-18 12:34:35 +0200

    fixed issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,28 +16,9 @@ data['drinks'].each do |drink|
   Ingredient.create(name: drink['strIngredient1'])
 end
 
-puts '> ### DONE (1/3) ###"
-
-# SH: Seed cocktails
-
-puts '> '
-puts '> '
-puts '> ### SEEDING COCKTAILS ###'
-
 20.times do
   cocktail = Cocktail.new(
     name: Faker::Science.element,
     )
-  puts cocktail.name
-  cocktail.save!
+  cocktail.save
 end
-
-puts '> ### DONE (2/3) ###'
-
-# SH: Seed doses
-
-puts '> '
-puts '> '
-puts '> ### SEEDING DOSES ###'
-
-puts '> ### DONE (3/3) ###'
